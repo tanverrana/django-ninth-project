d18380e6242d7bde49b5f134c0374f3af9df6709
pro <pro@prodeMacBook-Pro-2.local>
2023-09-04 11:51:20 +0600

    get cookie

--- first_app/views.py
+++ first_app/views.py
@@ -9,3 +9,9 @@ def home(request):
     response = render(request, 'home.html')
     response.set_cookie('name', 'tanver')
     return response
+
+
+def get_cookie(request):
+    name = request.COOKIES.get('name')
+    print(request.COOKIES)
+    return render(request, 'get_cookie.html', {'name': name})
